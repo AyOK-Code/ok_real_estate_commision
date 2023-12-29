f476d1692c98e60415b40cc2940526c2da42a77d
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2023-12-29 06:27:28 -0600

    Initial commit

---
+++ lib/ok_real_estate_commision.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ok_real_estate_commision/version"
+
+module OkRealEstateCommision
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ok_real_estate_commision_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OkRealEstateCommision do
+  it "has a version number" do
+    expect(OkRealEstateCommision::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
